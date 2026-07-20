c9e5f26f421a82b57c3504f81cfc826072437ffd
Faruk933 <130104726+Faruk933@users.noreply.github.com>
2026-07-20 11:44:03 +0100

    Add app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("EasyTasksz Started 🚀");
+
+    const watchBtn = document.getElementById("watchBtn");
+
+    if (watchBtn) {
+        watchBtn.addEventListener("click", () => {
+            alert("Watch Ads feature is coming soon!");
+        });
+    }
+});
